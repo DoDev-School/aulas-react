96598a620b78f30b067075b2d5cb5ace5a6ef292
Luiz Pachioni <lopachioni@gmail.com>
2023-10-31 17:48:16 -0300

    add operator button

--- pratica_1/src/App.tsx
+++ pratica_1/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
   const [firstNumber, setFirstNumber] = useState(0);
   const [secondNumber, setSecondNumber] = useState(0);
+  const [result, setResult] = useState(0);
 
   return (
     <>
@@ -22,8 +23,12 @@ function App() {
             onChange={e => setSecondNumber(Number(e.target.value))}
           />
         </label>
-        <span>Result: {firstNumber + secondNumber}</span>
+        <span>Result: {result}</span>
       </div>
+      <button onClick={() => setResult(firstNumber + secondNumber)}>+</button>
+      <button onClick={() => setResult(firstNumber - secondNumber)}>-</button>
+      <button onClick={() => setResult(firstNumber * secondNumber)}>*</button>
+      <button onClick={() => setResult(firstNumber / secondNumber)}>/</button>
     </>
   )
 }
